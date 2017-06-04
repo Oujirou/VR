91fa886d2c34f2424dc8231d0954d0c47ce4521e
Dan Peavey <danpeavey@gmail.com>
2017-06-03 21:24:18 -0700

    Adding bootstrapping.

---
+++ Assets/Scripts/Bootstrapper/GameBootstrapper.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace VRGame 
+{
+
+	public class GameBootstrapper : MonoBehaviour 
+	{
+		private void Start () 
+		{
+            SceneManager.LoadScene(GameScenes.GAME_SCENE, LoadSceneMode.Additive);
+			Debug.LogFormat("[{0}]: Loaded scene{1}.", this.GetType().Name, GameScenes.GAME_SCENE);
+		}
+	}
+
+}
