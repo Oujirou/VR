f7361372a087db35e8c130c8f79b33928ec0cc85
Dan Peavey <danpeavey@gmail.com>
2017-06-04 08:03:51 -0700

    Typo fix for log output.

--- Assets/Scripts/Bootstrapper/GameBootstrapper.cs
+++ Assets/Scripts/Bootstrapper/GameBootstrapper.cs
@@ -10,8 +10,8 @@ namespace VRGame
 	{
 		private void Start () 
 		{
-            SceneManager.LoadScene(GameScenes.GAME_SCENE, LoadSceneMode.Additive);
-			Debug.LogFormat("[{0}]: Loaded scene{1}.", this.GetType().Name, GameScenes.GAME_SCENE);
+			SceneManager.LoadScene(GameScenes.GAME_SCENE, LoadSceneMode.Additive);
+			Debug.LogFormat("[{0}]: Loaded scene {1}.", this.GetType().Name, GameScenes.GAME_SCENE);
 		}
 	}
 
